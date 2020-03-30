df97df4224b0d138e8fd899b0e646a2179b25ae6
Badal Moradia <badal@telnyx.com>
2020-03-30 11:55:53 -0500

    Track config file with git

---
+++ config.js
@@ -0,0 +1,10 @@
+let config = {};
+
+
+// The old key here has been deleted ;)
+
+// TODO: DELETE THESE KEYS
+config.telnyx_api_key = "YOUR_TELNYX_V2_API_KEY";
+config.game_number = "YOUR_TELNYX_PHONE_NUMBER";
+
+module.exports = config;
